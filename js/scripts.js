73674dfa1c57f8c3613c27886746335566cb4f41
Dinesh Swami <dineshswa@gmail.com>
2020-05-25 20:50:01 +0530

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,28 @@
-console.log('Hello World');
+const tableBody = document.querySelector("#homeWorkTableContiner table tbody");
+const tableRows = [];
+
+for(let i = 0; i < downloads.length; i++){
+    let row = `
+        <tr>
+            <th scope="row">${i + 1}</th>
+            <td>${downloads[i].date !== undefined && downloads[i].date !== '' ? downloads[i].date : '-'}</td>
+            <td>${downloads[i].description !== undefined && downloads[i].description !== '' ? downloads[i].description : '-'}</td>
+            <td>${downloads[i].subject !== undefined && downloads[i].subject !== '' ? downloads[i].subject : '-'}</td>
+            <td>
+                ${
+                    downloads[i].fileName !== undefined && downloads[i].fileName !== '' ? 
+                    `<a href="${downloads[i].downloadLink}${downloads[i].fileName}"
+                        title="${downloads[i].fileName}"
+                        target="_blank">
+                        ${downloads[i].fileName}
+                    </a>`
+                    : 'Download'
+                }
+            </td>
+        </tr>
+    `;
+    tableRows.push(row);
+}
+
+tableBody.innerHTML = tableRows.join('');
+
